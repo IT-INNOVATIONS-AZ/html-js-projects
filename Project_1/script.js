8a87fb51ab556dec35daf888c2d322fac05b557d
Vaqif Gulmammadov <vaqif14@gmail.com>
2022-02-20 23:41:07 +0400

    20.02.22

---
+++ Project_1/script.js
@@ -0,0 +1,16 @@
+const boxContainer = document.getElementById("boxes");
+const btn = document.getElementById("btn");
+
+btn.addEventListener("click", () => boxContainer.classList.toggle("big"));
+
+function createBoxes() {
+	for (let i = 0; i < 4; i++) {
+		for (let j = 0; j < 4; j++) {
+			const box = document.createElement("div");
+			box.classList.add("box");
+			box.style.backgroundPosition = `${-j * 125}px ${-i * 125}px`;
+			boxContainer.appendChild(box);
+		}
+	}
+}
+createBoxes();
